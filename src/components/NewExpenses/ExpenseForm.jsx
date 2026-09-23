463c404c8aa761429471ef61f1b2f1a1b85ed0a4
Nurruw-Meowthra <siimx164@gmail.com>
2026-09-23 06:20:21 +0000

    11. Vormitöötlus Reactis - userChangeHandler added

--- src/components/NewExpenses/ExpenseForm.jsx
+++ src/components/NewExpenses/ExpenseForm.jsx
@@ -2,21 +2,32 @@ import { useState } from 'react'
 import './ExpenseForm.css'
 
 const ExpenseForm = (props) => {
-    const [enteredTitle, setEnteredTitle] = useState('')
-    const [enteredPrice, setEnteredPrice] = useState('')
-    const [enteredDate, setEnteredDate] = useState('')
-    console.log(enteredTitle)
+    const [userInput, setuserInput] = useState({
+        enteredTitle: '',
+        enteredPrice: '',
+        enteredDate: ''
+    })
+    console.log(userInput)
 
     const titleChangeHandler = (event) => {
-        setEnteredTitle(event.target.value)
+        setuserInput({
+            ...userInput, 
+            enteredTitle: event.target.value
+        })
     }
 
     const priceChangeHandler = (event) => {
-        setEnteredPrice(event.target.value)
+        setuserInput({
+            ...userInput, 
+            enteredPrice: event.target.value
+        })
     }
 
     const dateChangeHandler = (event) => {
-        setEntereDate(event.target.value)
+        setuserInput({
+            ...userInput, 
+            enteredDate: event.target.value
+        })
     }
 
     return(
